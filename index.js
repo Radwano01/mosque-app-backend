2d49d9885f97a54c4b7df91f13062ec6cb99a278
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-18 02:46:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require("express")
 const cors = require("cors")
-const bodyparser = require("body-parser")
 const app = express()
 app.use(cors())
-app.use(bodyparser.json())
-const Data = require("./Data.json")
+app.use(express.json())
 
 const _PORT = 5001
 app.listen(_PORT, ()=>{
     console.log("server run at", _PORT)
 })
 
-app.post("/api/:country/:city/:month/:day/:weekday", (req, res) => {
+app.get("/api/:country/:city/:month/:day/:weekday", async(req, res) => {
     const { country, city, month, day, weekday } = req.params;
 
+    const Data = await fetch("https://radwano01.github.io/prayTimes-Data/Data.json")
     if(Data[country] && Data[country][city]){
         const countryTimes = Data[country]
         if(countryTimes){
             const cityTimes = countryTimes[city]
             if(cityTimes){
-                res.json(cityTimes[month][day][weekday])
+                res.status(200).json(cityTimes[month][day][weekday])
             }else{
                 res.status(404).json("no country exist with this")
             }
