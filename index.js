650692c5567aad758bf3869b171ca96e0ca03a4f
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-18 03:04:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config
 
-const _PORT = 5001
-app.listen(_PORT, ()=>{
-    console.log("server run at", _PORT)
-})
-
-app.get("/api/:country/:city/:month/:day/:weekday", async(req, res) => {
-    const { country, city, month, day, weekday } = req.params;
-
-    const Data = await fetch("https://radwano01.github.io/prayTimes-Data/Data.json")
-    if(Data[country] && Data[country][city]){
-        const countryTimes = Data[country]
-        if(countryTimes){
-            const cityTimes = countryTimes[city]
-            if(cityTimes){
-                res.status(200).json(cityTimes[month][day][weekday])
-            }else{
-                res.status(404).json("no country exist with this")
-            }
-        }else{
-            res.status(404).json("no country exist with this name")
-        }
-    }
+const _PORT = 5001;
+app.listen(_PORT, () => {
+  console.log("server run at", _PORT);
 });
 
+app.get("/api/:country/:city/:month/:day/:weekday", async (req, res) => {
+  const { country, city, month, day, weekday } = req.params;
+
+  try {
+    const response = await axios.get(process.env.DATA_API);
+    const Data = response.data;
+
+    if (Data[country] && Data[country][city]) {
+      const countryTimes = Data[country];
+
+      if (countryTimes) {
+        const cityTimes = countryTimes[city];
+
+        if (cityTimes) {
+          if (cityTimes[month] && cityTimes[month][day] && cityTimes[month][day][weekday]) {
+            res.status(200).json(cityTimes[month][day][weekday]);
+          } else {
+            res.status(404).json("No prayer times available for the specified date and weekday.");
+          }
+        } else {
+          res.status(404).json("No city exists with this name.");
+        }
+      } else {
+        res.status(404).json("No country exists with this name.");
+      }
+    } else {
+      res.status(404).json("No data available for the specified country and city.");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).json("Internal Server Error");
+  }
+});
